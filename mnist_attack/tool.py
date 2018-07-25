cac90c8d58ce3bab8fbcffde670dce1dc0f00fdf
Cfather <41474684+Cfather@users.noreply.github.com>
2018-07-25 16:11:54 +0800

    Update tool.py

--- mnist_attack/tool.py
+++ mnist_attack/tool.py
@@ -6,10 +6,11 @@ import torch.utils.data as Data
 import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
+import train_cnn
 
 def load_train_data():
     '''
-    USAGE: 直接使用，返回MNIST的训练集
+    USAGE: 直接使用，返回MNIST的原始训练集
     CONTRIBUTOR: 张博皓,2018/7/21
     '''
     train_data = tv.datasets.MNIST(root='data/mnist', train=True,
@@ -19,7 +20,7 @@ def load_train_data():
     
 def load_test_data():
     '''
-    USAGE: 直接使用，返回MNIST的验证集
+    USAGE: 直接使用，返回MNIST的原始验证集
     CONTRIBUTOR: 张博皓,2018/7/21
     '''
     test_data = tv.datasets.MNIST(root='data/mnist', train=False,
@@ -27,13 +28,19 @@ def load_test_data():
                                             download=True)
     return test_data
 
-def load_cnn(path='mnist_cnn_model.pkl'):
+def load_test_image():
     '''
-    USAGE: 直接使用，返回已经训练好的神经网络
+    USAGE: 直接使用，返回MNIST的验证集中的图片数组，维度为(10000,1,28,28)，每个像素点取值为[0,1]
     CONTRIBUTOR: 张博皓,2018/7/23
     '''
-    return tc.load(path)
+    return tc.tensor(load_test_data().test_data.reshape(10000,1,28,28),dtype=tc.float) / 255
 
+def load_test_label():
+    '''
+    USAGE: 直接使用，返回MNIST的验证集中的图片对应的真实标签
+    CONTRIBUTOR: 张博皓,2018/7/23
+    '''
+    return load_test_data().test_labels
 
 def imshow(instance, label):
     '''
@@ -43,5 +50,20 @@ def imshow(instance, label):
     '''
     plt.imshow(instance.reshape(28,28), cmap='gray')
     plt.title(label, fontsize = 20)
-    plt.show()
 
+def load_cnn_1():
+    '''加载训练好的CNN_1网络
+    CONTRIBUTOR: 张博皓,2018/7/25
+    '''
+    cnn = train_cnn.CNN_1()
+    cnn.load_state_dict(tc.load('mnist_CNN_1_model_params.pkl'))
+    return cnn
+    
+def load_cnn_2():
+    '''
+    USAGE: 加载训练好的CNN_1网络
+    CONTRIBUTOR: 张博皓,2018/7/25
+    '''
+    cnn = train_cnn.CNN_2()
+    cnn.load_state_dict(tc.load('mnist_CNN_2_model_params.pkl'))
+    return cnn
